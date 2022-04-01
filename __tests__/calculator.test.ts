2bbf71a75b2cd6186175bfecb34f5e1494beff3e
Felipe Gervasio <felipe_gerva@hotmail.com>
2022-04-01 09:18:26 -0300

    Reestruturação do projeto

--- __tests__/calculator.test.ts
+++ __tests__/calculator.test.ts
@@ -1,10 +1,10 @@
 import { driver, By2, windowsAppDriverCapabilities } from 'selenium-appium'
-import BasicCalculatorPage from '../Pages/BasicCalculatorPage';
-import MainPage from '../Pages/MainPage';
-import SumPage from '../Pages/SumPage';
-import SubtractionPage from '../Pages/SubtractionPage';
-import MultiplicationPage from '../Pages/MultiplicationPage';
-import DivisionPage from '../Pages/DivisionPage';
+import BasicCalculatorPage from '../operacoes/utils/BasicCalculatorPage';
+import MainPage from '../operacoes/utils/MainPage';
+import SumPage from '../operacoes/soma/SumPage';
+import SubtractionPage from '../operacoes/subtracao/SubtractionPage';
+import MultiplicationPage from '../operacoes/multiplicacao/MultiplicationPage';
+import DivisionPage from '../operacoes/divisao/DivisionPage';
 
 jest.setTimeout(50000);
 
@@ -19,59 +19,67 @@ afterAll(() => {
   return driver.quit();
 });
 
-describe('Testing Windows Calculator', () => {
+describe('Suite de testes - Windows Calculator', () => {
   beforeEach(async ()=> {
     await MainPage.clickZerarTudo(); // Garantindo que a calculadora esteja zerada.
     await BasicCalculatorPage.waitForPageLoaded();
   })
 
-  test('SumPage', async () => {
-    await SumPage.waitForPageLoaded();
+  describe('Suite de testes - Soma', () => {
+    test('Soma b', async () => {
+      await SumPage.waitForPageLoaded();
+  
+      await BasicCalculatorPage.clickN1Button();
+      await SumPage.clickSum1Button();
+      await BasicCalculatorPage.clickN7Button();
+  
+      await BasicCalculatorPage.gotoResult();
+  
+      expect(await BasicCalculatorPage.getResultOutput()).toBe('A exibição é 8');    
+    });
+  })
 
-    await BasicCalculatorPage.clickN1Button();
-    await SumPage.clickSum1Button();
-    await BasicCalculatorPage.clickN7Button();
+  describe('Suite de testes - Subtracao', () => {
+    test('SubtractionPage', async () => {
+      await SubtractionPage.waitForPageLoaded();
+  
+      await BasicCalculatorPage.clickN7Button();
+      await SubtractionPage.clickSubtraction1Button();
+      await BasicCalculatorPage.clickN1Button();
+  
+      await BasicCalculatorPage.gotoResult();
+  
+      expect(await BasicCalculatorPage.getResultOutput()).toBe('A exibição é 6');    
+    });
+  })
+  
+  describe('Suite de testes - Multiplicacao', () => {
+    test('MultiplicationPage', async () => {
+      await MultiplicationPage.waitForPageLoaded();
+  
+      await BasicCalculatorPage.clickN7Button();
+      await MultiplicationPage.clickMultiplication1Button();
+      await BasicCalculatorPage.clickN9Button();
+  
+      await BasicCalculatorPage.gotoResult();
+  
+      expect(await BasicCalculatorPage.getResultOutput()).toBe('A exibição é 63');    
+    });
+  })  
 
-    await BasicCalculatorPage.gotoResult();
-
-    expect(await BasicCalculatorPage.getResultOutput()).toBe('A exibição é 8');    
-  });
-
-  test('SubtractionPage', async () => {
-    await SubtractionPage.waitForPageLoaded();
-
-    await BasicCalculatorPage.clickN7Button();
-    await SubtractionPage.clickSubtraction1Button();
-    await BasicCalculatorPage.clickN1Button();
-
-    await BasicCalculatorPage.gotoResult();
-
-    expect(await BasicCalculatorPage.getResultOutput()).toBe('A exibição é 6');    
-  });
-
-  test('MultiplicationPage', async () => {
-    await MultiplicationPage.waitForPageLoaded();
-
-    await BasicCalculatorPage.clickN7Button();
-    await MultiplicationPage.clickMultiplication1Button();
-    await BasicCalculatorPage.clickN9Button();
-
-    await BasicCalculatorPage.gotoResult();
-
-    expect(await BasicCalculatorPage.getResultOutput()).toBe('A exibição é 63');    
-  });
-
-  test('DivisionPage', async () => {
-    await DivisionPage.waitForPageLoaded();
-
-    await BasicCalculatorPage.clickN7Button();
-    await BasicCalculatorPage.clickN0Button();
-    await DivisionPage.clickDivision1Button();
-    await BasicCalculatorPage.clickN1Button();
-    await BasicCalculatorPage.clickN0Button();
-
-    await BasicCalculatorPage.gotoResult();
-
-    expect(await BasicCalculatorPage.getResultOutput()).toBe('A exibição é 7');    
-  });
+  describe('Suite de testes - Divisao', () => {
+    test('DivisionPage', async () => {
+      await DivisionPage.waitForPageLoaded();
+  
+      await BasicCalculatorPage.clickN7Button();
+      await BasicCalculatorPage.clickN0Button();
+      await DivisionPage.clickDivision1Button();
+      await BasicCalculatorPage.clickN1Button();
+      await BasicCalculatorPage.clickN0Button();
+  
+      await BasicCalculatorPage.gotoResult();
+  
+      expect(await BasicCalculatorPage.getResultOutput()).toBe('A exibição é 7');    
+    });
+  })  
 });
